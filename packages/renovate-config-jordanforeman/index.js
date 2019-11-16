0c2cc7143103bc076d32a56807fd7b5dbbe60381
Jordan Foreman <me@jordanforeman.com>
2019-11-16 15:26:19 -0600

    fix(renovate): fix renovate module

--- packages/renovate-config-jordanforeman/index.js
+++ packages/renovate-config-jordanforeman/index.js
@@ -3,13 +3,8 @@
 module.exports = {
     extends: ['config:base'],
     masterIssue: true,
-    minor: {
-        automerge: true
-    },
-    patch: {
-        automerge: true
-    },
-    pin: {
-        automerge: true
+    automerge: true,
+    majord: {
+        automerge: false
     }
 };
