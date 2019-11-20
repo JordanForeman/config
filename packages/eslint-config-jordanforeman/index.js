c1b5b063cb505f904f9805a295f360de27923034
Jordan Foreman <me@jordanforeman.com>
2019-11-19 20:55:18 -0600

    fix(eslint): add custom rule overrides
    * Don't prefer default export ya crazy
    * indentation by fours

--- packages/eslint-config-jordanforeman/index.js
+++ packages/eslint-config-jordanforeman/index.js
@@ -4,5 +4,9 @@ module.exports = {
     extends: 'eslint-config-airbnb-base',
     env: {
         mocha: true
+    },
+    rules: {
+        'import/prefer-default-export': 0,
+        indent: ['error', 4]
     }
 };
