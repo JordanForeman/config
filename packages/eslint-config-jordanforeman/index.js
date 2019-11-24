2437cc0ff5d65c0bc47ec3c8cbde5dcb8fce5b6e
Jordan Foreman <me@jordanforeman.com>
2019-11-24 08:45:17 -0600

    fix(eslint): add more overrides

--- packages/eslint-config-jordanforeman/index.js
+++ packages/eslint-config-jordanforeman/index.js
@@ -7,6 +7,11 @@ module.exports = {
     },
     rules: {
         'import/prefer-default-export': 0,
-        indent: ['error', 4]
+        indent: ['error', 4],
+        'comma-dangle': ['error', 'never'],
+        'one-var': ['error', {
+            'let': 'consecutive',
+            'const': 'never'
+        }]
     }
 };
