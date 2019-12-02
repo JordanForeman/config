a9028a3a30ef31ca370558a96ec2aa9b962041d9
Jordan Foreman <me@jordanforeman.com>
2019-12-01 19:37:30 -0600

    fix(eslint): add react rules

--- packages/eslint-config-jordanforeman/index.js
+++ packages/eslint-config-jordanforeman/index.js
@@ -3,16 +3,24 @@
 module.exports = {
     extends: 'eslint-config-airbnb-base',
     parser: 'babel-eslint',
+    plugins: ['react'],
     env: {
         mocha: true
     },
     rules: {
         'import/prefer-default-export': 0,
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
         indent: ['error', 4],
         'comma-dangle': ['error', 'never'],
         'one-var': ['error', {
             'let': 'consecutive',
             'const': 'never'
         }]
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        }
     }
 };
